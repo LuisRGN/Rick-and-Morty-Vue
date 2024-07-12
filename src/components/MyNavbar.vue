c3b590885cac33af3451b9ad50f83d24a1b56388
LuisRGN <luisrafael1598@gmail.com>
2024-07-11 20:51:39 -0500

    se meten estilos de bootstrap al navbar y se ponen las redireciones al about y al home

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -1,16 +1,26 @@
 <template>
     <div>
-       <h1>{{ message }}</h1> 
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link to="/" class="navbar-brand">Mi App</router-link>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
     </div>
 </template>
 
 <script>
 export default {
     name: "MyNavbar",
-    data(){
-        return {
-            message: "Navbar"
-        }
-    }
+
 }
 </script>
