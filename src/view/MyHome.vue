126225a63c65852058a3df05c894ace2c2cfce73
LuisRGN <luisrafael1598@gmail.com>
2024-07-11 20:52:21 -0500

    se crea la vista del home y se le pasa un componente

---
+++ src/view/MyHome.vue
@@ -0,0 +1,22 @@
+<!-- src/view/MyHome.vue -->
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <MyComponente/>
+  </div>
+</template>
+
+<script>
+import MyComponente from '@/components/MyComponente.vue';
+
+export default {
+  name: 'MyHome',
+  components: {
+    MyComponente
+  }
+}
+</script>
+
+<style scoped>
+/* Estilos específicos para la vista Home */
+</style>
