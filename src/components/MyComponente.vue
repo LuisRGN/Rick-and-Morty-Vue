9d14b13ff772ccbcf8f71130a139fcc48a2bc5cf
LuisRGN <luisrafael1598@gmail.com>
2024-07-23 22:18:13 -0500

    se acomoda solo para mandar un mensaje desde el componente

--- src/components/MyComponente.vue
+++ src/components/MyComponente.vue
@@ -1,37 +1,22 @@
 <!-- src/components/MyComponente.vue -->
 <template>
   <div>
-    <h2>Hola desde MyComponente</h2>
-    <p :class="isValid">Este es un componente secundario.</p>
-    <button type="button" class="btn btn-secondary" @click="changeColor">Secondary</button>
+    <h2>Hola desde MyComponente</h2> 
   </div>
 </template>
 
 <script>
-import { ref, computed } from 'vue';
 
 export default {
   name: 'MyComponente',
   setup() {
-    const test = ref(true)
-    const changeColor = () => {
-      test.value = !test.value
-    }
+    
     return {
-      test,
-      changeColor,
-      isValid: computed(() => test.value ? 'red' : 'blue')
     }
   }
 }
 </script>
 
 <style scoped>
-.red {
-  color: red;
-}
 
-.blue {
-  color: blue;
-}
 </style>
