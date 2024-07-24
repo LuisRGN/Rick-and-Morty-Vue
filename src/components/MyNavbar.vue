dff952b0ece2c40d3508532f73c477f1753b5a16
LuisRGN <luisrafael1598@gmail.com>
2024-07-23 22:19:12 -0500

    se ajusta el navbar para dejar el logo a la izquierda y los links a la derecha y se pone un menu hamburguesa

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -1,26 +1,28 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <router-link to="/" class="navbar-brand">Mi App</router-link>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link">About</router-link>
-          </li>
-        </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <router-link to="/" class="navbar-brand">Mi App</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-    </div>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "MyNavbar",
-
+  name: "MyNavbar",
 }
 </script>
