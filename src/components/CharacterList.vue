eca88dc65046c47252b796837d798299c520b2f3
LuisRGN <luisrafael1598@gmail.com>
2024-08-06 20:01:01 -0500

    se quita la peticion y se ajusta el componente para usar el stores de pinia

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,7 +1,8 @@
+<!-- src/components/CharacterList.vue -->
 <template>
   <div class="container mt-4">
     <div class="row">
-      <div class="col-md-4" v-for="character in characters" :key="character.id">
+      <div class="col-md-4" v-for="character in filteredCharacters" :key="character.id">
         <div class="card mb-4 custom-card-color">
           <img :src="character.image" class="card-img-top" :alt="character.name">
           <div class="card-body">
@@ -39,54 +40,32 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { computed, onMounted } from 'vue';
+import { useCharacterStore } from '../stores/useFilterStore';
 
 export default {
   name: 'CharacterList',
   setup() {
-    const characters = ref([]);
-    const currentPage = ref(1);
-    const hasNext = ref(false);
-    const hasPrev = ref(false);
+    const store = useCharacterStore();
 
-    const fetchCharacters = async (page) => {
-      try {
-        const response = await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`);
-        characters.value = response.data.results;
-        hasNext.value = !!response.data.info.next;
-        hasPrev.value = !!response.data.info.prev;
-      } catch (error) {
-        console.error('Error fetching characters:', error);
-      }
-    };
+    const filteredCharacters = computed(() => store.filteredCharacters);
 
-    const nextPage = () => {
-      if (hasNext.value) {
-        currentPage.value++;
-        fetchCharacters(currentPage.value);
-      }
-    };
+    const nextPage = () => store.nextPage();
+    const prevPage = () => store.prevPage();
 
-    const prevPage = () => {
-      if (hasPrev.value) {
-        currentPage.value--;
-        fetchCharacters(currentPage.value);
-      }
-    };
-
-    onMounted(() => fetchCharacters(currentPage.value));
+    onMounted(() => store.fetchCharacters());
 
     return {
-      characters,
+      filteredCharacters,
       nextPage,
       prevPage,
-      hasNext,
-      hasPrev
+      hasNext: computed(() => store.hasNext),
+      hasPrev: computed(() => store.hasPrev),
     };
   }
 };
 </script>
+
 
 <style scoped>
 .card-img-top {
