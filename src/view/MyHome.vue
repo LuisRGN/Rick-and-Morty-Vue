8e0a8c6a7c0a247341d5ff151683e18315bcd6c3
LuisRGN <luisrafael1598@gmail.com>
2024-08-07 21:31:48 -0500

    se coloca el componente del filtro por status y se ajustan los estilos de los selectores

--- src/view/MyHome.vue
+++ src/view/MyHome.vue
@@ -1,20 +1,29 @@
 <!-- src/view/MyHome.vue -->
 <template>
-  <div>
-    <MyComponente/>
+  <div class="container mt-2">
+    <div class="row">
+      <div class="col-md-3">
+        <MyComponente/>
+      </div>
+      <div class="col-md-3">
+       <FilterForStatus/> 
+      </div>
+    </div>
     <CharacterList/>
   </div>
 </template>
 
 <script>
 import MyComponente from '@/components/MyComponente.vue';
-import CharacterList from '@/components/CharacterList.vue'
+import CharacterList from '@/components/CharacterList.vue';
+import FilterForStatus from '@/components/FilterForStatus.vue';
 
 export default {
   name: 'MyHome',
   components: {
     MyComponente,
-    CharacterList
+    CharacterList,
+    FilterForStatus
   }
 }
 </script>
