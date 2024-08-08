2397387801266a687dc3828070298128c313941b
LuisRGN <luisrafael1598@gmail.com>
2024-08-08 17:13:31 -0500

    se crea un componente para un buscador por el nombre

---
+++ src/components/SearchName.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <label class="form-label text-light">Search</label>
+        <input type="text" class="form-control custom-select" v-model="searchQuery" @input="updateSearch" placeholder="Search by name...">
+    </div>
+</template>
+
+<script>
+import {ref, watch} from 'vue';
+import { useCharacterStore } from '@/stores/useFilterStore';
+
+export default {
+    name: 'SearchBar',
+    setup(){
+        const store = useCharacterStore();
+        const searchQuery = ref(store.filters.name);
+
+        const updateSearch = () => {
+            store.setNameFilter(searchQuery.value);
+        }
+
+        watch(()=> store.filters.name, (newName => {
+            searchQuery.value = newName
+        }));
+        return {
+            searchQuery,
+            updateSearch
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.form-control {
+  border-radius: 5px;
+}
+.custom-select {
+  background-color: #8f8f8f;
+  color: #ffffff; 
+}
+.custom-select::placeholder {
+  color: #cccccc; 
+}
+.custom-select:focus {
+  background-color: #8f8f8f; 
+  color: #ffffff; 
+  border-color: #ffffff;
+}
+.custom-select:active {
+  background-color: #8f8f8f; 
+  color: #ffffff; 
+}
+.custom-select:not(:placeholder-shown) {
+  background-color: #6f6f6f;
+  color: #ffffff; 
+}
+</style>
