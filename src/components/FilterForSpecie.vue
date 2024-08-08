3c7187b88070d0ef9a26bde47064956439d21874
LuisRGN <luisrafael1598@gmail.com>
2024-08-07 21:29:14 -0500

    se crea un nuevo componente para el filtro por especie

---
+++ src/components/FilterForSpecie.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="from-group">
+
+    </div>
+</template>
+
+<script>
+import {ref, watch} from 'vue'
+import { useCharacterStore } from '../stores/useFilterStore'
+
+</script>
+
+<style scoped>
+
+</style>
