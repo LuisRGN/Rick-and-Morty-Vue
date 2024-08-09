a74ac3918ee0fc7066bf43ef00da6d5b42183b52
LuisRGN <luisrafael1598@gmail.com>
2024-08-09 13:55:44 -0500

    se acomoda el about  con una imagen y texto

--- src/view/MyAbout.vue
+++ src/view/MyAbout.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>
-      
+  <div class="container my-5 text-center">
+    <h2 class="text-light">Bienvenido a mi página de Rick and Morty echa con VUE</h2>
+    <p class="text-light">He creado esta página para practicar y explorar en profundidad las diversas funcionalidades que ofrece el framework Vue 3. A través de este proyecto, busco mejorar mis habilidades en el desarrollo de aplicaciones web modernas y responsivas, aprovechando las ventajas que ofrece Vue 3 en la construcción de interfaces dinámicas y eficientes</p>
+    <img :src="png" alt="png" class="img-fluid mb-4" style="width: 30rem;"/>
     </div>
-  </template>
+</template>
   
   <script>
+  import png from '@/assets/img/pngwing.com.png'
   export default {
-    name: 'MyAbout'
+    name: 'MyAbout',
+    setup(){
+      return {
+        png
+      }
+    }
   }
   </script>
+
+  <style scoped>
+
+  </style>
