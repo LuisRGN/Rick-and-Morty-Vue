ddac8bcd563a4248818af67ff8285a524cb70ded
LuisRGN <luisrafael1598@gmail.com>
2024-08-09 13:54:34 -0500

    se agrega una imagen al navbar y se acomodan los tamaños de texto

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg">
       <div class="container">
-        <router-link to="/" class="navbar-brand">Mi App</router-link>
+        <router-link to="/" class="navbar-brand">
+          <img :src="rm" alt="rm" style="width: 5rem;">
+        </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
+              <router-link to="/" class="nav-link fs-3" aria-current="page">Home</router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/about" class="nav-link">About</router-link>
+              <router-link to="/about" class="nav-link fs-3">About</router-link>
             </li>
           </ul>
         </div>
@@ -22,7 +24,13 @@
 </template>
 
 <script>
+import rm from '@/assets/img/walpape.png'
 export default {
   name: "MyNavbar",
+  setup(){
+    return{
+      rm
+    }
+  }
 }
 </script>
