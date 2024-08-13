a1604e421e9f175b49ff341e11b7684404727c2c
LuisRGN <luisrafael1598@gmail.com>
2024-08-13 14:20:35 -0500

    se crea un componente para un 404

---
+++ src/components/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container mt-2 my-5 text-center">
+        <img :src="cuatro" alt="404" class="img-fluid mb-4 rounded-3" style="width: 55rem; height: 35rem;">
+    </div>
+</template>
+
+<script>
+import cuatro from '@/assets/img/404.jpeg'
+export default {
+    name: 'NotFound',
+    setup(){
+        return {
+            cuatro
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
