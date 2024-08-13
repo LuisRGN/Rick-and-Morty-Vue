6c667621b37567e343e780bff1933b864c756647
LuisRGN <luisrafael1598@gmail.com>
2024-08-13 14:20:22 -0500

    se crea un componente para el spinner creado con estilos de boostrap

---
+++ src/components/MySpinner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center mt-4">
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'MySpinner',
+}
+
+</script>
+
+<style scoped>
+    .spinner-border{
+        width: 3rem;
+        height: 3rem;
+        border-width: 0.25em;
+    }
+</style>
