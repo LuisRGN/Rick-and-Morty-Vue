a0cfde2925c4b6953c268268d8b2d5e556ce0f95
LuisRGN <luisrafael1598@gmail.com>
2024-08-13 14:19:44 -0500

    se crea un componente para limpiar los campos

---
+++ src/components/ClearFilters.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="characterStore.clearFilters" type="button" class="btn btn-dark">Limpiar</button>
+    </div>
+</template>
+
+<script>
+import { useCharacterStore } from '@/stores/useFilterStore';
+
+export default {
+    name: 'ClearFilter',
+    setup() {
+        const characterStore = useCharacterStore(); 
+        return { characterStore };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
