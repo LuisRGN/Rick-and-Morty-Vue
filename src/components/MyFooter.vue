006340fad1f4a5442cfaa65a29bbbd145f22ad31
LuisRGN <luisrafael1598@gmail.com>
2024-08-13 14:11:19 -0500

    se acomoda el footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-dark text-white py-3 mt-5">
       <div class="container text-center">
-        <p class="mb-0">© 2024 Mi Página Web. Todos los derechos reservados.</p>
+        <p class="mb-0">© 2024 Luis Gonzalez. Todos los derechos reservados.</p>
         <p>
           <a href="#" class="text-white text-decoration-none">Política de Privacidad</a> | 
           <a href="#" class="text-white text-decoration-none">Términos de Servicio</a>
