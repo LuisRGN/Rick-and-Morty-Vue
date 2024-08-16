77f16b43035fd0444dae06f6486f05bf161731ac
LuisRGN <luisrafael1598@gmail.com>
2024-08-15 19:06:46 -0500

    se crea una nueva vista para mostrar a un personaje con mas detalles

---
+++ src/view/CharacterDetail.vue
@@ -0,0 +1,62 @@
+<template>
+    <div v-if="loading">
+        <MySpinner />
+    </div>
+    <div>
+        <div class="container" v-if="characterDetail && !loading">
+            <div class="d-flex flex-column align-items-center">
+                <h1>{{ characterDetail.name }}</h1>
+                <img :src="characterDetail.image" :alt="characterDetail.name" class="img-fluid mb-4">
+                <h6>Status: {{ characterDetail.status }}</h6>
+                <h6>Species: {{ characterDetail.species }}</h6>
+                <h6>Gender: {{ characterDetail.gender }}</h6>
+                <h6>Origin: {{ characterDetail.origin.name }}</h6>
+                <h6>Location: {{ characterDetail.location.name }}</h6>
+            </div>
+            <div v-if="episodes.length">
+                <h5 class="text-center">Episodes</h5>
+                <div class="row justify-content-center">
+                    <div v-for="episode in episodes" :key="episode.id" class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <h6 class="text-center">{{ episode.name }}</h6>
+                        <p class="text-center">{{ episode.air_date }} | Episode: {{ episode.episode }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+import { useCharacterStore } from '@/stores/useFilterStore';
+import { onMounted, computed } from 'vue';
+import MySpinner from '@/components/MySpinner.vue';
+
+export default {
+    name: 'CharacterDetail',
+    components: {
+        MySpinner
+    },
+    setup() {
+        const route = useRoute()
+        const store = useCharacterStore()
+        const characterDetail = computed(() => store.characterDetails)
+        const loading = computed(() => store.loading);
+        const episodes = computed(() => store.episodes);
+        const loadingEpisodes = computed(() => store.loadingEpisodes);
+
+        onMounted(() => {
+            const characterId = route.params.id
+            store.fetchCharacterById(characterId)
+        })
+        return {
+            characterDetail,
+            loading,
+            episodes,
+            loadingEpisodes,
+        }
+    }
+}
+</script>
+
+<style scoped></style>
