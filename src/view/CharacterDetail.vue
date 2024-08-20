e8424361a870e91ec50fddc95bddee1cd882ba6e
LuisRGN <luisrafael1598@gmail.com>
2024-08-19 22:42:23 -0500

    se ajustan los episodios con un bg y tamaños iguales y todo el texto en blanco

--- src/view/CharacterDetail.vue
+++ src/view/CharacterDetail.vue
@@ -3,7 +3,7 @@
         <MySpinner />
     </div>
     <div>
-        <div class="container" v-if="characterDetail && !loading">
+        <div class="container text-light" v-if="characterDetail && !loading">
             <div class="d-flex flex-column align-items-center mt-4">
                 <img :src="characterDetail.image" :alt="characterDetail.name" class="img-fluid mb-4 rounded">
                 <h1>{{ characterDetail.name }}</h1>
@@ -15,10 +15,12 @@
             </div>
             <div v-if="episodes.length">
                 <h5 class="text-center">Episodes</h5>
-                <div class="row justify-content-center">
+                <div class="row justify-content-center ">
                     <div v-for="episode in episodes" :key="episode.id" class="col-sm-6 col-md-4 col-lg-3 mb-4">
-                        <h6 class="text-center">{{ episode.name }}</h6>
-                        <p class="text-center">{{ episode.air_date }} | Episode: {{ episode.episode }}</p>
+                        <div class="episode-card bg-secondary rounded shadow-lg">
+                            <h6 class="text-center">{{ episode.name }}</h6>
+                            <p class="text-center">{{ episode.air_date }} | Episode: {{ episode.episode }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -59,4 +61,12 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.episode-card {
+    min-height: 150px; 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px;
+}
+</style>
