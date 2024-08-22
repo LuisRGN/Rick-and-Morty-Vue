97d69ccec837890c7bd36a2020ca3e83ffc24bb3
LuisRGN <luisrafael1598@gmail.com>
2024-08-22 13:03:21 -0500

    se crea un nuevo componente para reresar al home cuando ves el detalle del personaje

---
+++ src/components/ReturnButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center mt-3">
+        <router-link to="/">
+          <button type="button" class="btn btn-dark">HOME</button>  
+        </router-link>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'ReturnButton'
+}
+
+</script>
+
+<style scoped>
+
+</style>
