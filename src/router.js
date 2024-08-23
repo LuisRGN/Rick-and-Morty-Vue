f123e0cbc49d453ff10d6d0adc801151546a51b5
LuisRGN <luisrafael1598@gmail.com>
2024-08-23 11:50:51 -0500

    se crea una nueva ruta

--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MyHome from "./view/MyHome.vue"
 import MyAbout from "./view/MyAbout.vue"
 import CharacterDetail from './view/CharacterDetail.vue'
+import MyFavorite from './view/MyFavorite.vue'
 
 const routes = [
     { path: '/', component: MyHome },
     { path: '/about', component: MyAbout },
-    { path: '/characterdetail/:id', name: 'characterdetail', component: CharacterDetail }
+    { path: '/characterdetail/:id', name: 'characterdetail', component: CharacterDetail },
+    { path: '/favorite', component: MyFavorite }
 ]
 
 const router = createRouter({
